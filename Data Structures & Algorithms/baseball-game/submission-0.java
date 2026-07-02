d5ac7492b01eee2fe9ed56d171bf906b1a2bc2a1
Ankush Gemnani <amazingankush2714@gmail.com>
2026-07-02 17:43:20 +0530

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.java
@@ -0,0 +1,41 @@
+
+class Solution {
+    public int calPoints(String[] operations) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : operations) {
+
+            if (op.equals("C")) {
+
+                stack.pop();
+
+            } else if (op.equals("D")) {
+
+                int score = stack.peek();
+                stack.push(2 * score);
+
+            } else if (op.equals("+")) {
+
+                int first = stack.pop();
+                int second = stack.peek();
+
+                stack.push(first);
+                stack.push(first + second);
+
+            } else {
+
+                stack.push(Integer.parseInt(op));
+
+            }
+        }
+
+        int sum = 0;
+
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+
+        return sum;
+    }
+}
