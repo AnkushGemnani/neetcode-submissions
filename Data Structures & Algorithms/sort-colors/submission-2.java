3e50966de085d3e7b412ab12db233c4a65749b67
Ankush Gemnani <amazingankush2714@gmail.com>
2026-08-17 13:01:14 +0530

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+        // Count frequency
+        for (int num : nums) { //for(int i=0;i<n;i++)
+            if (num == 0)
+                zero++;
+            else if (num == 1)
+                one++;
+            else
+                two++;
+        }
+        int index = 0;
+        // Fill 0s
+        while (zero-- > 0) {
+            nums[index++] = 0;
+        }
+        // Fill 1s
+        while (one-- > 0) {
+            nums[index++] = 1;
+        }
+        // Fill 2s
+        while (two-- > 0) {
+            nums[index++] = 2;
+        }
+    }
+}
